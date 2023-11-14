2b6c918385c1762398a452c13494dfd556423a65
RitaPintoBissApp <rpinto@blissapplications.com>
2023-11-14 15:18:47 +0000

    Grid complete

--- app/src/main/java/com/example/grid_app/MainActivity.kt
+++ app/src/main/java/com/example/grid_app/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,6 +29,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.grid_app.data.DataSource
 import com.example.grid_app.model.Topic
 import com.example.grid_app.ui.theme.Grid_AppTheme
 
@@ -37,13 +43,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TopicGridApp()
                 }
             }
         }
     }
 }
 
+@Composable
+fun TopicGridApp() {
+    TopicGrid(tipsList = DataSource.topics, modifier = Modifier)
+}
+@Composable
+fun TopicGrid( tipsList: List<Topic>, modifier: Modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier){
+        items(tipsList){topic ->
+            Griditem(
+                topic = topic,
+                modifier = Modifier.padding(8.dp))
+        }
+    }
+}
 
 
 @Composable
@@ -54,16 +76,19 @@ fun Griditem(topic: Topic, modifier: Modifier = Modifier) {
                painter = painterResource(topic.imageResourceId) ,
                contentDescription = stringResource(topic.stringResourceId),
                modifier = Modifier
-                   .heightIn(68.dp),
+                   .size(width = 68.dp, height = 68.dp)
+                   .aspectRatio(1f),
                contentScale = ContentScale.Crop
            )
            Column(modifier.padding(
+               start = 0.dp,
+               bottom = 0.dp,
                top = 16.dp,
                end = 16.dp,
            )) {
                Text(
                    text = LocalContext.current.getString(topic.stringResourceId),
-                   fontSize = 15.sp,
+                   fontSize = 11.sp,
                    modifier = Modifier
                        .padding(
                            start = 16.dp,
@@ -79,17 +104,14 @@ fun Griditem(topic: Topic, modifier: Modifier = Modifier) {
                        painter = painterResource(R.drawable.ic_grain) ,
                        contentDescription = stringResource(topic.stringResourceId),
                        modifier = Modifier
-                           .padding(
-                               start = 16.dp,
-                               bottom = 2.dp,
-                           )
-                           .heightIn(10.dp),
+                           .padding(start = 16.dp)
+                           .heightIn(20.dp),
                        contentScale = ContentScale.Fit,
 
                    )
                    Text(
                        text = topic.numberResourceId.toString(),
-                       fontSize = 12.sp,
+                       fontSize = 10.sp,
                        modifier = Modifier.padding(
                            top = 2.dp,
                            start = 8.dp),
@@ -108,6 +130,6 @@ fun Griditem(topic: Topic, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Grid_AppTheme {
-Griditem( Topic(R.string.architecture,58,R.drawable.architecture ))
-    }
+       TopicGridApp()
+ }
 }
